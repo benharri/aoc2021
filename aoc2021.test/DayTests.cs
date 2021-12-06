93e61904ac932d63089ac6d0659aef866d2d2e98
Ben Harris <ben@tilde.team>
2021-12-06 00:16:42 -0500

    day 6 part 1

--- aoc2021.test/DayTests.cs
+++ aoc2021.test/DayTests.cs
@@ -44,6 +44,7 @@ public class DayTests
     [DataRow(typeof(Day03), "198", "230")]
     [DataRow(typeof(Day04), "4512", "1924")]
     [DataRow(typeof(Day05), "5", "12")]
+    [DataRow(typeof(Day06), "5934", "")]
     public void CheckTestInputs(Type dayType, string part1, string part2)
     {
         Day.UseTestInput = true;
---
+++ aoc2021/Day06.cs
@@ -0,0 +1,43 @@
+﻿namespace aoc2021;
+
+/// <summary>
+/// Day 6: <see href="https://adventofcode.com/2021/day/6"/>
+/// </summary>
+public sealed class Day06 : Day
+{
+    private readonly List<int> _fishes;
+    public Day06() : base(6, "Lanternfish")
+    {
+        //UseTestInput = true;
+        _fishes = Input.First().Split(',').Select(int.Parse).ToList();
+    }
+
+    private static List<int> DayStep(List<int> state)
+    {
+        List<int> result = new();
+        
+        foreach (var fish in state)
+        {
+            switch (fish)
+            {
+                case 0:
+                    result.Add(6);
+                    result.Add(8);
+                    break;
+                default:
+                    result.Add(fish - 1);
+                    break;
+            }
+        }
+
+        return result;
+    }
+
+    public override string Part1()
+    {
+        var fishes = Enumerable.Range(0, 80).Aggregate(_fishes, (current, _) => DayStep(current));
+        return $"{fishes.Count}";
+    }
+
+    public override string Part2() => "";
+}
